19d3ca5719ef4027116874a0fdc774f3465ac8cf
Thrisha1701 <saithrisha123@gmail.com>
2025-12-04 10:27:35 +0530

    Third commit with new package

---
+++ src/basic_002_decision_making_statements/A001_if.java
@@ -0,0 +1,10 @@
+package basic_002_decision_making_statements;
+
+public class A001_if {
+	public static void main(String[] args) {
+		int a=6;
+		if(a>0) {
+			System.out.println(a+ "is positive");
+		}
+	}
+}

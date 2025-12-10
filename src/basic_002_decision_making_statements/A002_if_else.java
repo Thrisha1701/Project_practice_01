ca7492ea594708a1430c1b753804b65bbcb33d06
pamulacharan <missionsuccessforever@gmail.com>
2025-12-10 08:46:20 +0530

    # WARNING: head commit changed in the meantime
    8th commit
    
    Change-Id: Ifbf54cb641a772882695a415ee23bc633bc95abf

---
+++ src/basic_002_decision_making_statements/A002_if_else.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package basic_002_decision_making_statements;
+
+/**
+ * @author charan sbi withdraw
+ *
+ */
+public class A002_if_else {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int money=1000;
+		if (money>500) {
+			System.out.println("with draw logic will work");
+		} else {
+			System.out.println("with draw logic don't work because insufficient balences");
+		}
+
+	}
+
+}

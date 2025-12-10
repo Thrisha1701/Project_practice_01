319df9b3dbf7a8d611d7d26b40e4aa9c91ec7a7d
pamulacharan <missionsuccessforever@gmail.com>
2025-12-10 09:26:55 +0530

    11th commit

---
+++ src/basic_002_decision_making_statements/A003_sample.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package basic_002_decision_making_statements;
+
+/**
+ * @author lenovo
+ *
+ */
+public class A003_sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
